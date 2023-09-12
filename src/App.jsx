03f8620deeaf9df8f5e5a91834fb62c7028b517e
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-09-12 17:04:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import RandomQuote from './components/RandomQuote'
+
+function App() {
+
+  return (
+    // <RandomContextProvider>
+     <RandomQuote />
+    // </RandomContextProvider>
+  )
+}
+
+export default App
---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export const baseURL = 'https://api.api-ninjas.com/v1/quotes?category='
+
+export const config = {
+  headers: {
+    'X-Api-Key': 'G3uOhZVPxleXQXr4YeY1MA==DcCcFzEo6lYg2JyX'
+  }
+}
+
+export const options = ['age', 'alone', 'amazing', 'attitude', 'car', 'funny', 'good', 'love', 'money', 'success', 'movies', 'leadership', 'mom', 'men']
